e4605e88c0a2421a2588c397c5f17669d8088676
Omer Can Baykara <omercanbaykara@gmail.com>
2025-01-19 14:22:43 +0300

    Test now works from any filepath

--- tests/test.zsh
+++ tests/test.zsh
@@ -1,11 +1,14 @@
 #!/usr/bin/env zsh
 
 files=("full.txt" "locale.txt" "whitespace.txt")
+script_dir="$(dirname "$0")"
+
 for file in ${files[@]}; do
+    full_path="$script_dir/$file"
 
-    echo "Testing $file"
+    echo "Testing $full_path"
 
-    if diff <(wc "$file" | tr -s ' ') <(my_wc "$file" | tr -s ' '); then
+    if diff <(wc "$full_path" | tr -s ' ') <(my_wc "$full_path" | tr -s ' '); then
         echo "✓ Test passed for $file."
     else
         echo "✗ Test failed for $file."
